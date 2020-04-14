fb3d0a506fe72b410092015a028c6653dd102272
trias832 <58136565+trias832@users.noreply.github.com>
2020-04-14 12:24:24 +0700

    Add files via upload
    responsi pemrograman

---
+++ Karyawan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2733
+{
+   public class Karyawan
+    {
+        //Constructor
+        public Karyawan(int nik,string nama,int gajiBulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            if(gajiBulanan < 0)
+            {
+                GajiBulanan = 0;
+                Naik = Convert.ToInt32((GajiBulanan * 1.1));
+            }
+            else
+            {
+                GajiBulanan = gajiBulanan;
+                Naik = Convert.ToInt32((GajiBulanan * 1.1));
+            }
+
+        }
+
+
+        //properties
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        public int Naik { get; set; }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2733
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("No       NIK             NAMA                GAJI BULANAN                ");
+            Console.WriteLine("=========================================================================");
+            //membuat objek Karyawan 1
+            Karyawan kry1 = new Karyawan(1903423243,"trias", 3000000);
+            Console.WriteLine("1.       {0}             {1}                 {2}                         ",kry1.Nik,kry1.Nama,kry1.GajiBulanan);
+            //membuat objek karyawan2
+            Karyawan kry2 = new Karyawan(1902348623, "handayani", 200000);
+            Console.WriteLine("2.       {0}             {1}             {2}                         ", kry2.Nik, kry2.Nama, kry2.GajiBulanan);
+
+            Console.WriteLine("\n\nAsyik kenaikan gaji 10%");
+            Console.WriteLine("No       NIK             NAMA                GAJI BULANAN                ");
+            Console.WriteLine("=========================================================================");
+            Console.WriteLine("1.       {0}             {1}                 {2}                         ", kry1.Nik, kry1.Nama,kry1.Naik);
+            Console.WriteLine("2.       {0}             {1}             {2}                         ", kry2.Nik, kry2.Nama, kry2.Naik);
+
+
+            Console.ReadKey();
+        }
+      
+    }
+}
